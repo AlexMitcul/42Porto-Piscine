e117730ddb94c5a7678705cb35714568117b5d7b
Alexandru Mitcul <alexmitcul@Alexandrus-Air.lan>
2022-10-04 00:29:57 +0100

    ex00

---
+++ C11/ex00/ft_foreach.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_foreach.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alexmitcul <alexmitcul@student.42.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/04 00:22:15 by alexmitcul        #+#    #+#             */
+/*   Updated: 2022/10/04 00:29:02 by alexmitcul       ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_foreach(int *tab, int length, void(*f)(int))
+{
+	int	i;
+
+	i = 0;
+	while (i < length)
+	{
+		f(tab[i]);
+		i++;
+	}
+}
+
+/* #include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int x)
+{
+	ft_putchar(x + '0');
+}
+
+int	main(void)
+{
+	int	length = 10;
+	int	arr[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+	ft_foreach(arr, length, &ft_putnbr);
+	return (0);
+} */
