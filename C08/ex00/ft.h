c0286d8e44f563363f8dec5492d67a13d731cf30
Alex Mitcul <amitcul@student.42porto.com>
2022-10-04 22:04:39 +0100

    C08 | 70%

---
+++ C08/ex00/ft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amitcul <amitcul@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/29 16:52:38 by amitcul           #+#    #+#             */
+/*   Updated: 2022/09/29 16:54:17 by amitcul          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_H
+# define FT_H
+
+void	ft_putchar(char c);
+void	ft_swap(int *a, int *b);
+void	ft_putstr(char *str);
+int		ft_strlen(char *str);
+int		ft_strcmp(char *s1, char *s2);
+
+#endif
---
+++ C08/ex04/ft_strs_to_tab.c
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strs_to_tab.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amitcul <amitcul@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/29 17:19:40 by amitcul           #+#    #+#             */
+/*   Updated: 2022/10/02 15:53:17 by amitcul          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+#include "ft_stock_str.h"
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(char *src)
+{
+	int		i;
+	char	*res;
+
+	i = 0;
+	res = (char *)malloc(ft_strlen(src) * sizeof(char) + 1);
+	if (!res)
+		return (0);
+	while (src[i])
+	{
+		res[i] = src[i];
+		i++;
+	}
+	res[i] = '\0';
+	return (res);
+}
+
+struct s_stock_str	*ft_strs_to_tab(int ac, char **av)
+{
+	struct s_stock_str	*arr;
+	int					i;
+
+	arr = malloc((ac + 1) * sizeof(struct s_stock_str));
+	if (!arr)
+		return (NULL);
+	i = 0;
+	while (i < ac)
+	{
+		arr[i].size = ft_strlen(av[i]);
+		arr[i].str = av[i];
+		arr[i].copy = ft_strdup(av[i]);
+		i++;
+	}
+	arr[i].size = 0;
+	arr[i].str = 0;
+	arr[i].copy = 0;
+	return (arr);
+}
